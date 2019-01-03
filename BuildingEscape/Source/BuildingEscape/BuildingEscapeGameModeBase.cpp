dff8be04799005c1581c2f3e728e80c3c80063fd
subotich <suboti.ch@icloud.com>
2019-01-03 23:03:23 +0100

    Working local copy rewrite & upgrade UE4.21
    Updated generated source files

--- BuildingEscape/Source/BuildingEscape/BuildingEscapeGameModeBase.cpp
+++ BuildingEscape/Source/BuildingEscape/BuildingEscapeGameModeBase.cpp
@@ -1,8 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "BuildingEscapeGameModeBase.h"
 #include "BuildingEscape.h"
-#include "BuildingEscapeGameMode.h"
-
-
-
 
